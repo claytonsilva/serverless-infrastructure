8652a5884542e50722f2bc6262cc4af68f54c766
claytonsilva <clayton@xdevel.com.br>
2018-12-08 12:48:13 -0200

    feat: add backend lock table

--- management/backend.tf
+++ management/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    encrypt = "false"
-    bucket  = "crypto-resources-terraform"
-    key     = "management/terraform.tfstate"
-    region  = "us-east-2"
-    profile = "backend"
+    encrypt        = "false"
+    bucket         = "crypto-resources-terraform"
+    key            = "management/terraform.tfstate"
+    dynamodb_table = "terraform_locks"
+    region         = "us-east-2"
+    profile        = "backend"
   }
 }
