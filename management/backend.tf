0fcfc76d5965ec2c5cefda3d741829620db6aa18
Rafael Favaro <rafael.faesin@gmail.com>
2019-01-26 16:56:27 -0200

    feat: add state

--- management/backend.tf
+++ management/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
     encrypt        = "false"
-    bucket         = "crypto-resources-terraform"
+    bucket         = "crypto-resources-terraform-faesin"
     key            = "management/terraform.tfstate"
     dynamodb_table = "terraform_locks"
     region         = "us-east-2"
-    profile        = "backend"
+    profile        = "default"
   }
 }
--- management/s3-lambda.tf
+++ management/s3-lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "lambda" {
-  bucket = "crypto-lambda-repository"
+  bucket = "crypto-lambda-repository-faesin"
   acl    = "private"
 
   lifecycle_rule {
